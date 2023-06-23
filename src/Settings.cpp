13fa3b8f497850f05d0c5f1c1a0b4db8869df12e
BlackyDrum <ganiaytan1010@web.de>
2023-06-23 17:49:26 +0200

    Add collapsing header for each section

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -4,26 +4,29 @@ void Settings::settings(bool& showSettings, float& movementSpeed, float& jumpFor
 {
 	ImGui::Begin("Settings", &showSettings);
 
-	ImGui::Text("Player Settings");
-	ImGui::SliderFloat("Movement Speed", &movementSpeed, 1, 15);
-	ImGui::SliderFloat("Jump Force", &jumpForce, 10, 50);
-    ImGui::SliderFloat("Rapid Fire Speed", &projectileFireSpeed, 0.05, 1.0);
+    if (ImGui::CollapsingHeader("Player Settings"))
+    {
+        ImGui::SliderFloat("Movement Speed", &movementSpeed, 1, 15);
+        ImGui::SliderFloat("Jump Force", &jumpForce, 10, 50);
+        ImGui::SliderFloat("Rapid Fire Speed", &projectileFireSpeed, 0.05, 1.0);
 
-	ImGui::NewLine();
+    }
 
-	ImGui::Text("World Settings");
-	ImGui::SliderFloat("Gravity", &gravity ,0.1, 1.0);
+    if (ImGui::CollapsingHeader("World Settings"))
+    {
+        ImGui::SliderFloat("Gravity", &gravity, 0.1, 1.0);
+    }
+	
+    if (ImGui::CollapsingHeader("Tile Settings"))
+    {
+        ImGui::SliderFloat("Feather Force", &featherForce, 30, 100);
+        ImGui::SliderFloat("Trampoline Force", &trampolineForce, 40, 100);
+    }
 
-	ImGui::NewLine();
-
-	ImGui::Text("Tile Settings");
-	ImGui::SliderFloat("Feather Force", &featherForce, 30, 100);
-	ImGui::SliderFloat("Trampoline Force", &trampolineForce, 40, 100);
-
-	ImGui::NewLine();
-
-	ImGui::Text("Assets");
-	ImGui::SliderInt("Volume", &volume, 0, 100);
+    if (ImGui::CollapsingHeader("Assets"))
+    {
+        ImGui::SliderInt("Volume", &volume, 0, 100);
+    }
 
     ImGui::NewLine();
 
